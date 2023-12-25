505139565bc3c48642664197ccf363117612de9d
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2023-12-25 03:15:09 +0100

    add some files for building model

--- DWProject/src/main/java/com/DW/group/DWProject/App.java
+++ DWProject/src/main/java/com/DW/group/DWProject/App.java
@@ -27,8 +27,12 @@ public class App
         int primaryKeyJoueur = 1 ;
         int primaryKeyEntaineur = 1 ;
         int primaryKeyCity = 1 ;
+        int primarykeyStadium = 1 ;
+        int primaryKeyTeam = 1 ;
 		int IdCoach = 0 ;
 		int IdCaptain = 0 ;
+		int idSignature = 1 ;
+		int dateDebut = 1943 ;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		SimpleDateFormat dateformatyear = new SimpleDateFormat("yyyy");
 		
@@ -42,61 +46,77 @@ public class App
         		FileWriter csvCity = new FileWriter("generateData/City.csv");
         		FileWriter csvStadium = new FileWriter("generateData/Stadium.csv");
         		FileWriter csvCountry = new FileWriter("generateData/Country.csv") ;
-//        		FileWriter csvSeason = new FileWriter("generateData/Season.csv");
+        		FileWriter csvSeason = new FileWriter("generateData/Season.csv");
 //        		FileWriter csvMatch = new FileWriter("generateData/Match.csv");
 //        		FileWriter csvGoal = new FileWriter("generateData/Goal.csv");
-//        		FileWriter csvCompetition = new FileWriter("generateData/Competition.csv");
+        		FileWriter csvCompetition = new FileWriter("generateData/Competition.csv");
 //        		FileWriter csvParticiper = new FileWriter("generateData/Participer.csv");
-//        		FileWriter csvSignature = new FileWriter("generateData/Signature.csv")
+        		FileWriter csvSignature = new FileWriter("generateData/Signature.csv")
         				){
         	
          	csvCountry.append("idCountry , CountryName\n");
-        	csvCity.append("idCountry , CityName , idCountry\n");
-        	csvStadium.append("idStudiam , capacity , libelle , long , lat , idCity\n") ;
+        	csvCity.append("idCity , CityName , idCountry\n");
+        	csvStadium.append("idStadium , libelle , totalPlaces ,  address , idCity\n") ;
 //        	
-//         	csvSeason.append("idSeason , datedebut , datefin\n");
-//        	csvMatch.append("idMatch , date_Time , idHomeTeam , idAwayTeam , idStadium , idSeason\n");
+         	csvSeason.append("idSeason , datedebut , datefin\n");
+//        	csvMatch.append("idMatch , date_Time , idHomeTeam , idAwayTeam , idStadium , idCompetition , idSeason\n");
 //        	csvGoal.append("idGoal , timeGoal , ownGoal , idMatch , idButteur , idPasseur , idTypeGoal\n") ;
 //        	
 			csvJoueur.append("idJoueur , FirstName , LastName , dob ,idCountry, idPosition\n");
 			csvEntraineur.append("idEntraineur , FirstName , LastName , dob ,idCountry , idEntraineurPost\n");
 //			
 			csvTeam.append("idTeam , TeamName , createAt , idCaptain , idEntraineur\n");
-//			csvCompetition.append("idComprtition , Libelle , idCountry , Prize , idSeason , idTeamWin\n");
+			csvCompetition.append("idComprtition , Libelle , idCountry , Prize , idSeason , idTeamWin\n");
 //			csvParticiper.append("idParticiper , idTeam , idCompetition\n");
 //			
-//			csvSignature.append("idSign ,dateSignature , dateExpiration , Montant , idTeam , idPerson\n");
+			csvSignature.append("idSign ,dateSignature , dateExpiration , Montant , idTeam , idPerson\n");
 
 			for (int j = 1; j <= 80; j++) {
 				Address address = faker.address();
 				csvCountry.append(String.format("%d,%s\n", j , address.country()));
-
-				for (int i = 1; i <= 10 ; i++) {
+ 				csvSeason.append(String.format("%d,%s,%s\n", j , dateDebut++ , dateDebut++));
+ 				csvCompetition.append(String.format("%d,"));
+ 				for (int i = 1; i <= 10 ; i++) {
+ 					
 					csvCity.append(String.format("%d,%s,%d\n", primaryKeyCity++ , address.city() , j ));
-	 				csvStadium.append(String.format("%d,%d,%s,\n" , primaryKeyCity , faker.number().numberBetween(0, 100000) ,
-	 						address.city() + " Stade" , address.longitude() , address.latitude()));
+					
+	 				csvStadium.append(String.format("%d,%s,%s,%s,%d\n" , primarykeyStadium++ , address.city() + " Stade" ,
+	 						faker.number().numberBetween(0, 100000) , address.lastName()+ address.city() + address.country() ,
+	 						primaryKeyCity-1));
+	 				
 	 				int numberOfPlayers = 0 ;
 	 				while ( numberOfPlayers++ <= 15) {
 	 					Name name = faker.name();
 						csvJoueur.append(String.format("%d,%s,%s,%s,%d,%d\n" , primaryKeyJoueur++ , name.firstName() , name.lastName() ,
 								dateFormat.format(faker.date().birthday(15, 41 )) , j , faker.number().numberBetween(1, 7)));
-	 					
+						DateAndTime dateSignExp = faker.date();
+	 					csvSignature.append(String.format("%d,%s,%s,%d,%d,%d\n", idSignature++ , dateSignExp.birthday(0,4) ,
+	 							dateSignExp.future(700, java.util.concurrent.TimeUnit.DAYS) , faker.number().numberBetween(2000 , 7000000) , primaryKeyTeam , primaryKeyJoueur));
 					}
 	 				IdCaptain+=15;
 	 				
 					csvEntraineur.append(String.format("%d,%s,%s,%s,%d,%d\n", primaryKeyEntaineur++ , faker.name().firstName() , faker.name().lastName() ,
 							dateFormat.format(faker.date().birthday(39, 70 )) , j , 1));
+					DateAndTime dateSignExp = faker.date();
+					csvSignature.append(String.format("%d,%s,%s,%d,%d,%d\n", idSignature++ , dateSignExp.birthday(0,4) ,
+ 							dateSignExp.future(700, java.util.concurrent.TimeUnit.DAYS) , faker.number().numberBetween(2000 , 7000000) , primaryKeyTeam , primaryKeyEntaineur));
+			
+					
 	 				int numberOfCoachInTeam = 0 ;
 	 				while (numberOfCoachInTeam++ <= 4) {
 						csvEntraineur.append(String.format("%d,%s,%s,%s,%d,%d\n", primaryKeyEntaineur++ , faker.name().firstName() , faker.name().lastName() ,
 								dateFormat.format(faker.date().birthday(39, 70 )) ,faker.number().numberBetween(1, 80)  , faker.number().numberBetween(1, 7)));
 						
+						DateAndTime dateSignExp2 = faker.date();
+						csvSignature.append(String.format("%d,%s,%s,%d,%d,%d\n", idSignature++ , dateSignExp2.birthday(0,4) ,
+	 							dateSignExp2.future(700, java.util.concurrent.TimeUnit.DAYS) , faker.number().numberBetween(2000 , 7000000) , primaryKeyTeam , primaryKeyEntaineur));
+				
 					}
 	 				IdCoach+=4;
 	 				
 	 				Date dateCreateAt = faker.date().birthday(23, 200) ;
 	 				
-	 				csvTeam.append(String.format("%d,%s,%s,%d,%d\n",j,faker.team().name() + "football Club " + dateformatyear.format(dateCreateAt) 
+	 				csvTeam.append(String.format("%d,%s,%s,%d,%d\n",primaryKeyTeam++,faker.team().name() + " football Club " + dateformatyear.format(dateCreateAt) 
 	 				, dateFormat.format(dateCreateAt) , IdCaptain , IdCoach  ));
 	 				
 				}
--- DWProject/src/main/java/com/DW/group/DWProject/Test.java
+++ DWProject/src/main/java/com/DW/group/DWProject/Test.java
@@ -9,14 +9,18 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Faker faker = new Faker(new Locale("en"));
-		System.out.println(faker.zelda().game());
+		System.out.println(java.util.concurrent.TimeUnit.DAYS);
  		System.out.println(faker.date().birthday().toString());
- 		int j = 0 ;
- 		while (j <= 16) {
-			System.out.println(j +" mmm");
-			j++;
-			
-		}
+ 		System.out.println(faker.date().birthday(0, 4));
+  		System.out.println(faker.date().past(29200 , java.util.concurrent.TimeUnit.DAYS));
+ 		System.out.println(faker.number().numberBetween(2000 , 7000000));
+ 		System.out.println(faker.address().fullAddress());
+// 		int j = 0 ;
+// 		while (j <= 16) {
+//			System.out.println(j +" mmm");
+//			j++;
+//			
+//		}
 	}
 
 }
